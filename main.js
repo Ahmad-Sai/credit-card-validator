77947eb14811bbb3b0a957ea9ef86e0784cf7319
Ahmad-Sai <85080576+Ahmad-Sai@users.noreply.github.com>
2022-07-10 19:34:51 -0600

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9];
 const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6];
 const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5];
 const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6];
+
 // All invalid credit card numbers
 const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5];
 const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3];
@@ -22,29 +23,64 @@ const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3];
 const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5];
 
 function validateCred(arr){
-  let arr2 = [...arr];
-  for(let i =arr2.length-2; i >= 0; i-=2){
-    if(arr2[i]*2 > 9){
-      arr2[i] = (arr2[i]*2)-9;
+  let card = [...arr];
+  
+  // Luhn algorithm (card number validation algorithim)
+  for(let i =card.length - 2; i >= 0; i -= 2){
+    if(card[i] * 2 > 9){
+      card[i] = (card[i] * 2) - 9;
     } else{
-      arr2[i] = (arr2[i]*2);
+      card[i] = (card[i] * 2);
     }
   }
-  let c = 0;
-  for(let i =0; i<arr2.length; i++){
-      c += arr2[i];
-    }
-  if(c%10 === 0){
-    return 'valid';
+  
+  let counter = 0;
+  for(let i = 0; i < card.length; i++){
+    counter += card[i];
+  }
+  
+  if(counter % 10 === 0){
+    return 1;
   } else{
-    return 'invalid';
-    }
+    return 0;
+  }
 }
 
+//console.log(validateCred(invalid3));  testing the validator function
+
+// find all invalid cards within the bunch
 function findInvalidCards(arr){
-  return arr.filter((element) => validateCred(element) === 'invalid');
+  return arr.filter((element) => validateCred(element) === 0);
 }
 
+let invalids = findInvalidCards(batch);
+console.log(invalids);
 
 
+// check which companies issued an invalid cards
+function idInvalidCardCompanies(invalids){
+  let invalidCardCompanies = [];
+  let amex = 'Amex (American Express)';
+  let visa = 'Visa';
+  let mastercard = 'Mastercard';
+  let discover = 'Discover';
 
+  for(let i =0; i< invalids.length; i++){
+    if (invalids[i][0] === 3 && invalidCardCompanies.indexOf(amex) === -1){
+      invalidCardCompanies.push(amex);
+    } else if(invalids[i][0] === 4 && invalidCardCompanies.indexOf(visa) === -1){
+        invalidCardCompanies.push(visa);
+    } else if (invalids[i][0] === 5 && invalidCardCompanies.indexOf(mastercard) === -1){
+          invalidCardCompanies.push(mastercard);
+    } else if (invalids[i][0] === 6 && invalidCardCompanies.indexOf(discover) === -1) {
+        invalidCardCompanies.push(discover);
+    } else{
+      console.log("Company not found");
+    }
+  }
+    return invalidCardCompanies; 
+}
+
+let cardCompanies = idInvalidCardCompanies(invalids);
+
+console.log(cardCompanies);
